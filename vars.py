c28026f5215af1cde098d90c775c75070cadc906
GittyBits <imailtausif@gmail.com>
2025-07-18 17:44:55 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -10,10 +10,10 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "1591043415"))
 CREDIT = environ.get("CREDIT", "Marcus")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '1591043415').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '1591043415,1722652154').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '1591043415').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '1591043415,1722652154').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
