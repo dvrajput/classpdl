a12486a24219b1daeac52c8c093d0e2d4eeada0f
Vishvrajsinh <86516265+dvrajput@users.noreply.github.com>
2025-07-26 16:48:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "1922259"))
+API_HASH = environ.get("API_HASH", "df46b4bda20f16abe680979c9639d702")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7422356439:AAE_6CcVFsKp8R9oE_4PGUQFZi9XC3trX1s")
 
-OWNER = int(environ.get("OWNER", "1591043415"))
-CREDIT = environ.get("CREDIT", "Marcus")
+OWNER = int(environ.get("OWNER", "1722652154"))
+CREDIT = environ.get("CREDIT", "dv1910")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '1591043415,1722652154').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
